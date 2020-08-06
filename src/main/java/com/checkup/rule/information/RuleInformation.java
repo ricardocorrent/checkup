ac44fd675c1dc0513144c8e0ddfbf551f71abc59
Ricardo Corrent <ricardocorrent@hotmail.com>
2020-08-06 20:57:05 -0300

    fixing rule and rule information

--- src/main/java/com/checkup/rule/information/RuleInformation.java
+++ src/main/java/com/checkup/rule/information/RuleInformation.java
@@ -2,8 +2,10 @@ package com.checkup.rule.information;
 
 import com.checkup.server.model.BaseInformation;
 
+import javax.persistence.Entity;
 import javax.persistence.Table;
 
+@Entity
 @Table(name = "rule_information", schema = "checkup")
 public class RuleInformation extends BaseInformation {
 }
