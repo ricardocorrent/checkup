c8214ce2272907f2742bcf15dc33336c080ac1a4
Ricardo Corrent <ricardocorrent@hotmail.com>
2020-11-12 20:16:46 -0300

    Fixing rule list all

--- src/main/java/com/checkup/inspection/InspectionRepository.java
+++ src/main/java/com/checkup/inspection/InspectionRepository.java
@@ -1,10 +1,22 @@
 package com.checkup.inspection;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.UUID;
 
 @Repository
 public interface InspectionRepository extends JpaRepository<Inspection, UUID> {
+
+    @Override
+    @Query("select i from Inspection i order by i.draft desc")
+    Page<Inspection> findAll(Pageable pageable);
+
+    @Override
+    @Query("select i from Inspection i order by i.draft desc")
+    List<Inspection> findAll();
 }
