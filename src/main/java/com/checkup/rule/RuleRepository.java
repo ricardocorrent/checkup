23242cbead973104b3f080d2e16aeffac7678848
Ricardo Corrent <ricardocorrent@hotmail.com>
2020-11-12 20:02:06 -0300

    Fixing rule list all

--- src/main/java/com/checkup/rule/RuleRepository.java
+++ src/main/java/com/checkup/rule/RuleRepository.java
@@ -1,9 +1,16 @@
 package com.checkup.rule;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.PagingAndSortingRepository;
 
 import java.util.UUID;
 
 @org.springframework.stereotype.Repository
 public interface RuleRepository extends PagingAndSortingRepository<Rule, UUID> {
+
+    @Override
+    @Query("select r from Rule r where r.cloned = false")
+    Page<Rule> findAll(Pageable pageable);
 }
